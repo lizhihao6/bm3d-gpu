b9d83cc19809ccf79fb05b634f121153075b036a
lizhihao6 <lizhihao6@outlook.com>
2023-12-11 20:24:00 +0800

    change develop into install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 work_path=$(dirname $(readlink -f $0))
 cd ${work_path}/pytorch_bm3d/
-python setup.py develop | grep "error"
+python setup.py install | grep "error"
 cd ../
-python setup.py develop
+python setup.py install
